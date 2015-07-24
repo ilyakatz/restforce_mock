5c89fd45957a0a3743c73087a8d4b2531f620ddb
Ilya Katz <ilyakatz@users.noreply.github.com>
2015-07-24 17:39:45 -0400

    Update restforce_mock_spec.rb

--- spec/restforce_mock_spec.rb
+++ spec/restforce_mock_spec.rb
@@ -4,8 +4,4 @@ describe RestforceMock do
   it 'has a version number' do
     expect(RestforceMock::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
