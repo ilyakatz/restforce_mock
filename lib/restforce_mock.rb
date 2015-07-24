c149d5dd6f4933f45acb8c33cb89c4c60802ff8f
Ilya Katz <ilyakatz@gmail.com>
2015-07-24 17:23:09 -0400

    add the actual initial code

--- lib/restforce_mock.rb
+++ lib/restforce_mock.rb
@@ -1,5 +1,64 @@
 require "restforce_mock/version"
+require "restforce"
 
 module RestforceMock
+  class Client
+
+    include ::Restforce::Concerns::API
+
+    def initialize
+      @storage = Hash.new do |hash, object|
+        hash[object]={}
+      end
+    end
+
+    def api_patch(url, attrs)
+      url=~/sobjects\/(.+)\/(.+)/
+      object=$1
+      id=$2
+      validate_presence!(object, id)
+      update_object(object, id, attrs)
+    end
+
+    def api_post(url, attrs)
+      url=~/sobjects\/(.+)/
+      sobject = $1
+      id = SecureRandom.urlsafe_base64(13) #duplicates possible
+      add_object(sobject, id, attrs)
+      return Body.new(id)
+    end
+
+    def validate_presence!(object, id)
+      unless storage[object][id]
+        raise Faraday::Error::ResourceNotFound.new("Provided external ID field does not exist or is not accessible: #{id}")
+      end
+    end
+
+    def add_object(name, id, values)
+      if @storage[name][id].present?
+        raise "Object #{name} with #{id} exists"
+      end
+      @storage[name].merge!({ id  => values })
+    end
+
+    def update_object(name, id, attrs)
+      current = @storage[name][id]
+      @storage[name][id] = current.merge(attrs)
+    end
+
+    def storage
+      @storage
+    end
+
+    class Body
+      def initialize(id)
+        @body = {'id' => id}
+      end
+
+      def body
+        @body
+      end
+    end
+  end
   # Your code goes here...
 end
--- restforce_mock.gemspec
+++ restforce_mock.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Ilya Katz"]
   spec.email         = ["ilyakatz@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Mock for Restforce gem}
+  spec.description   = %q{Mock for Restforce gem}
+  spec.homepage      = "https://github.com/ilyakatz"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "restforce"
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
