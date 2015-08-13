f62abb96b3fb7d841d8f3ef657e8313ad0d8d994
Ilya Katz <ilyakatz@gmail.com>
2015-08-13 10:31:38 -0400

    just want to see what hound says

--- lib/restforce_mock.rb
+++ lib/restforce_mock.rb
@@ -1,12 +1,16 @@
 require "restforce_mock/version"
-require "restforce_mock/sandbox"
 require "restforce"
 
 module RestforceMock
   class Client
 
     include ::Restforce::Concerns::API
-    include RestforceMock::Sandbox
+
+    def initialize
+      @storage = Hash.new do |hash, object|
+        hash[object]={}
+      end
+    end
 
     def api_patch(url, attrs)
       url=~/sobjects\/(.+)\/(.+)/
@@ -25,12 +29,26 @@ module RestforceMock
     end
 
     def validate_presence!(object, id)
-      unless RestforceMock::Sandbox.storage[object][id]
+      unless storage[object][id]
         raise Faraday::Error::ResourceNotFound.new("Provided external ID field does not exist or is not accessible: #{id}")
       end
     end
 
-    private
+    def add_object(name, id, values)
+      if @storage[name][id].present?
+        raise "Object #{name} with #{id} exists"
+      end
+      @storage[name].merge!({ id  => values })
+    end
+
+    def update_object(name, id, attrs)
+      current = @storage[name][id]
+      @storage[name][id] = current.merge(attrs)
+    end
+
+    def storage
+      @storage
+    end
 
     class Body
       def initialize(id)
@@ -42,4 +60,5 @@ module RestforceMock
       end
     end
   end
+  # Your code goes here...
 end
