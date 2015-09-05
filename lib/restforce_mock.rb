0e19596bcf7ca8048d786b1828634e844e7c180d
Ilya Katz <ilyakatz@gmail.com>
2015-09-05 09:56:30 -0400

    configure restforce_mock in it's own file

--- lib/restforce_mock.rb
+++ lib/restforce_mock.rb
@@ -4,3 +4,20 @@ require "restforce_mock/client"
 require "restforce_mock/sandbox"
 require "restforce_mock/schema_manager"
 
+module RestforceMock
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= RestforceMock::Configuration.new
+  end
+
+  def self.reset
+    @configuration = RestforceMock::Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
--- lib/restforce_mock/configuration.rb
+++ lib/restforce_mock/configuration.rb
@@ -3,21 +3,3 @@ module RestforceMock
     attr_accessor :schema_file
   end
 end
-
-module RestforceMock
-  class << self
-    attr_writer :configuration
-  end
-
-  def self.configuration
-    @configuration ||= RestforceMock::Configuration.new
-  end
-
-  def self.reset
-    @configuration = RestforceMock::Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-end
